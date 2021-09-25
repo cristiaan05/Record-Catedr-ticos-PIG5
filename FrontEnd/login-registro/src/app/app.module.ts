be6cb292ab1af8faea489fa1365ef68a8f394f68
luisfergr <3707090630101@ingenieria.usac.edu.gt>
2021-09-25 00:05:25 -0600

    cambio contraseña front

--- FrontEnd/login-registro/src/app/app.module.ts
+++ FrontEnd/login-registro/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { RegistroComponent } from './registro/registro.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    ChangePasswordComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
